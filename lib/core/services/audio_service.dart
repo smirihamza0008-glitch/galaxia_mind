2c0a234780ddda3437f9232c47a8bbfc41c77b12
smirihamza0008-glitch <smirihamza0008@gmail.com>
2026-05-16 16:38:23 +0100

    Create audio_service.dart

---
+++ lib/core/services/audio_service.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/services.dart';
+
+class AudioService {
+  static void playQuantumTap() {
+    SystemSound.play(SystemSoundType.click);
+  }
+
+  static void playSuccessLetterPulse() {
+    HapticFeedback.selectionClick();
+  }
+
+  static void playNavigationError() {
+    HapticFeedback.vibrate();
+  }
+
+  static void playGalaxyWinSequence() {
+    HapticFeedback.successImpact();
+  }
+}
